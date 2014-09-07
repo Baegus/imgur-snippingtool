1b064b9dea493c9db3bb1875ab8124b70291712f
Jaroslav Petrnoušek <baegus@gmail.com>
2014-09-07 11:49:05 +0200

    Update MainFrom.cs

--- snippingtool/MainFrom.cs
+++ snippingtool/MainFrom.cs
@@ -28,154 +28,11 @@ using System.Drawing.Imaging;
 using System.Net;
 using System.Xml;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
-using Microsoft.Win32;
 
 namespace snippingtool
 {
-	public static class FlashWindow
-{
-[DllImport("user32.dll")]
-[return: MarshalAs(UnmanagedType.Bool)]
-private static extern bool FlashWindowEx(ref FLASHWINFO pwfi);
 
-[StructLayout(LayoutKind.Sequential)]
-private struct FLASHWINFO
-{
-/// <summary>
-/// The size of the structure in bytes.
-/// </summary>
-public uint cbSize;
-/// <summary>
-/// A Handle to the Window to be Flashed. The window can be either opened or minimized.
-/// </summary>
-public IntPtr hwnd;
-/// <summary>
-/// The Flash Status.
-/// </summary>
-public uint dwFlags;
-/// <summary>
-/// The number of times to Flash the window.
-/// </summary>
-public uint uCount;
-/// <summary>
-/// The rate at which the Window is to be flashed, in milliseconds. If Zero, the function uses the default cursor blink rate.
-/// </summary>
-public uint dwTimeout;
-}
 
-/// <summary>
-/// Stop flashing. The system restores the window to its original stae.
-/// </summary>
-public const uint FLASHW_STOP = 0;
-
-/// <summary>
-/// Flash the window caption.
-/// </summary>
-public const uint FLASHW_CAPTION = 1;
-
-/// <summary>
-/// Flash the taskbar button.
-/// </summary>
-public const uint FLASHW_TRAY = 2;
-
-/// <summary>
-/// Flash both the window caption and taskbar button.
-/// This is equivalent to setting the FLASHW_CAPTION | FLASHW_TRAY flags.
-/// </summary>
-public const uint FLASHW_ALL = 3;
-
-/// <summary>
-/// Flash continuously, until the FLASHW_STOP flag is set.
-/// </summary>
-public const uint FLASHW_TIMER = 4;
-
-/// <summary>
-/// Flash continuously until the window comes to the foreground.
-/// </summary>
-public const uint FLASHW_TIMERNOFG = 12;
-/// <summary>
-/// Flash the spacified Window (Form) until it recieves focus.
-/// </summary>
-/// <param name=”form”>The Form (Window) to Flash.</param>
-/// <returns></returns>
-public static bool Flash(System.Windows.Forms.Form form)
-{
-// Make sure we’re running under Windows 2000 or later
-if (Win2000OrLater)
-{
-FLASHWINFO fi = Create_FLASHWINFO(form.Handle, FLASHW_ALL | FLASHW_TIMERNOFG, uint.MaxValue, 0);
-return FlashWindowEx(ref fi);
-}
-return false;
-}
-
-private static FLASHWINFO Create_FLASHWINFO(IntPtr handle, uint flags, uint count, uint timeout)
-{
-FLASHWINFO fi = new FLASHWINFO();
-fi.cbSize = Convert.ToUInt32(Marshal.SizeOf(fi));
-fi.hwnd = handle;
-fi.dwFlags = flags;
-fi.uCount = count;
-fi.dwTimeout = timeout;
-return fi;
-}
-
-/// <summary>
-/// Flash the specified Window (form) for the specified number of times
-/// </summary>
-/// <param name=”form”>The Form (Window) to Flash.</param>
-/// <param name=”count”>The number of times to Flash.</param>
-/// <returns></returns>
-public static bool Flash(System.Windows.Forms.Form form, uint count)
-{
-if (Win2000OrLater)
-{
-FLASHWINFO fi = Create_FLASHWINFO(form.Handle, FLASHW_ALL, count, 0);
-return FlashWindowEx(ref fi);
-}
-return false;
-}
-
-/// <summary>
-/// Start Flashing the specified Window (form)
-/// </summary>
-/// <param name=”form”>The Form (Window) to Flash.</param>
-/// <returns></returns>
-public static bool Start(System.Windows.Forms.Form form)
-{
-if (Win2000OrLater)
-{
-FLASHWINFO fi = Create_FLASHWINFO(form.Handle, FLASHW_ALL, uint.MaxValue, 0);
-return FlashWindowEx(ref fi);
-}
-return false;
-}
-
-/// <summary>
-/// Stop Flashing the specified Window (form)
-/// </summary>
-/// <param name=”form”></param>
-/// <returns></returns>
-public static bool Stop(System.Windows.Forms.Form form)
-{
-if (Win2000OrLater)
-{
-FLASHWINFO fi = Create_FLASHWINFO(form.Handle, FLASHW_STOP, uint.MaxValue, 0);
-return FlashWindowEx(ref fi);
-}
-return false;
-}
-
-/// <summary>
-/// A boolean value indicating whether the application is running on Windows 2000 or later.
-/// </summary>
-private static bool Win2000OrLater
-{
-get { return System.Environment.OSVersion.Version.Major >= 5; }
-}
-}
-	
     public partial class MainForm : Form
     {
         private const string API_KEY = "830bececb56919ddd399ee27d45ffec4";
@@ -187,19 +44,17 @@ get { return System.Environment.OSVersion.Version.Major >= 5; }
             progressBar1.Maximum = data.max_value;
             progressBar1.Value = data.value;
         }
-        
-        
+
         public void uploadCompleteThreadSafe(snippingtool.Imgur.UploadResults results)
         {
-        	
             imgur_textbox.Text = results.imgur_page;
             original_textbox.Text = results.original;
             delete_textbox.Text = results.delete_page;
-            Clipboard.SetText(results.original);
-            copied_label.Text = String.Format(COPIED_CLIPBOARD, "Original");
+
+            Clipboard.SetText(results.imgur_page);
+            copied_label.Text = String.Format(COPIED_CLIPBOARD, "Imgur");
             tabControl1.SelectedTab = tabPage2;
             ((Control)this.tabPage2).Enabled = true;
-            FlashWindow.Flash(this);
         }
 
         /// <summary>
@@ -234,7 +89,7 @@ get { return System.Environment.OSVersion.Version.Major >= 5; }
         {
             if (!backgroundWorker1.IsBusy)
             {
-            	this.Opacity = .0;
+                this.Opacity = .0; // Hides dialog window while selecting 
                 progressBar1.Value = 0;
                 backgroundWorker1.RunWorkerAsync();
             }
@@ -243,7 +98,7 @@ get { return System.Environment.OSVersion.Version.Major >= 5; }
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
             var bmp = SnippingTool.Snip();
-this.Opacity = 1;
+            this.Opacity = 1; // Shows dialog window while uploading
             if (bmp != null)
             {
                 try
@@ -264,11 +119,11 @@ this.Opacity = 1;
 
             switch (box.Text)
             { 
+                case "Imgur":
+                    Process.Start(imgur_textbox.Text);
+                    break;
                 case "Original":
                     Process.Start(original_textbox.Text);
-                    break;
-            	case "Imgur":
-                    Process.Start(imgur_textbox.Text);
                     break;
                 case "Delete":
                     Process.Start(delete_textbox.Text);
@@ -285,11 +140,11 @@ this.Opacity = 1;
             
             switch (box.Name)
             { 
-                case "original_textbox":
-                    copied_label.Text = String.Format(COPIED_CLIPBOARD, "Original");
-                    break;
                 case "imgur_textbox":
                     copied_label.Text = String.Format(COPIED_CLIPBOARD, "Imgur");
+                    break;
+                case "original_textbox":
+                    copied_label.Text = String.Format(COPIED_CLIPBOARD, "Original");
                     break;
                 case "delete_textbox":
                     copied_label.Text = String.Format(COPIED_CLIPBOARD, "Delete");
